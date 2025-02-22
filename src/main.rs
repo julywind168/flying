e3b2eaec69300a9fa0a94c3c08a0bccbf4159d0c
windy <707298413@qq.com>
2025-02-23 01:27:37 +0800

    add actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix::prelude::*;
+
+#[derive(Message)]
+#[rtype(result = "usize")]
+struct Sum(usize, usize);
+
+struct Calculator;
+
+impl Actor for Calculator {
+    type Context = Context<Self>;
+}
+
+impl Handler<Sum> for Calculator {
+    type Result = usize;
+    fn handle(&mut self, msg: Sum, _: &mut Self::Context) -> Self::Result {
+        msg.0 + msg.1
+    }
+}
+
+#[actix::main] // <- starts the system and block until future resolves
+async fn main() {
+    let addr = Calculator.start();
+    let res = addr.send(Sum(10, 5)).await; // <- send message and get future for result
+
+    match res {
+        Ok(result) => println!("SUM: {}", result),
+        _ => println!("Communication to the actor has failed"),
+    }
 }
