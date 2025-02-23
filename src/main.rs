9ff5dad6e02e4a28bd9945723c21e60f9679b2c1
windy <707298413@qq.com>
2025-02-23 17:56:02 +0800

    add mlua

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix::prelude::*;
+use mlua::prelude::*;
 
 #[derive(Message)]
 #[rtype(result = "usize")]
@@ -19,6 +20,17 @@ impl Handler<Sum> for Calculator {
 
 #[actix::main] // <- starts the system and block until future resolves
 async fn main() {
+    let lua = Lua::new();
+
+    let map_table = lua.create_table().unwrap();
+    map_table.set(1, "one").unwrap();
+    map_table.set("two", 2).unwrap();
+
+    lua.globals().set("map_table", map_table).unwrap();
+
+    lua.load("for k,v in pairs(map_table) do print(k,v) end")
+        .exec().unwrap();
+
     let addr = Calculator.start();
     let res = addr.send(Sum(10, 5)).await; // <- send message and get future for result
 
