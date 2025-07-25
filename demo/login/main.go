16d2adad96c3a1557102e9c195ea3d79c80f14dd
windy <707298413@qq.com>
2025-07-26 00:03:24 +0800

    update demo: add proto

--- demo/login/main.go
+++ demo/login/main.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"net/http"
 
+	"github.com/julywind168/flying/demo/proto"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -15,7 +16,8 @@ func Start() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", hello)
+	e.POST("/login", login)
+	e.POST("/register", register)
 
 	// Start server
 	if err := e.Start(":9999"); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -24,6 +26,30 @@ func Start() {
 }
 
 // Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func login(c echo.Context) error {
+	var payload proto.LoginPayload
+	if err := c.Bind(&payload); err != nil {
+		return c.JSON(http.StatusBadRequest, proto.Result{
+			Code:    proto.ErrCodeBadRequest,
+			Message: err.Error(),
+		})
+	}
+	return c.JSON(http.StatusOK, proto.Result{
+		Code:    proto.ErrCodeSuccess,
+		Message: "Login successful",
+	})
+}
+
+func register(c echo.Context) error {
+	var payload proto.RegisterPayload
+	if err := c.Bind(&payload); err != nil {
+		return c.JSON(http.StatusBadRequest, proto.Result{
+			Code:    proto.ErrCodeBadRequest,
+			Message: err.Error(),
+		})
+	}
+	return c.JSON(http.StatusOK, proto.Result{
+		Code:    proto.ErrCodeSuccess,
+		Message: "Register successful",
+	})
 }
---
+++ demo/proto/error.go
@@ -0,0 +1,43 @@
+package proto
+
+type ErrCode uint32
+
+const (
+	ErrCodeSuccess ErrCode = iota + 1
+	ErrCodeUnknown
+
+	// 客户端错误（1xxx）
+	ErrCodeBadRequest ErrCode = 1000 + iota
+	ErrCodeInvalidParams
+	ErrCodeUnauthorized
+	ErrCodeForbidden
+	ErrCodeNotFound
+
+	// 服务端错误（2xxx）
+	ErrCodeInternal ErrCode = 2000 + iota
+	ErrCodeNotImplemented
+	ErrCodeServiceUnavailable
+
+	// 业务错误（3xxx+）
+)
+
+// 错误码映射描述
+var errorMessages = map[ErrCode]string{
+	ErrCodeSuccess:            "success",
+	ErrCodeUnknown:            "unknown error",
+	ErrCodeBadRequest:         "bad request",
+	ErrCodeInvalidParams:      "invalid parameters",
+	ErrCodeUnauthorized:       "unauthorized",
+	ErrCodeForbidden:          "forbidden",
+	ErrCodeNotFound:           "not found",
+	ErrCodeInternal:           "internal server error",
+	ErrCodeNotImplemented:     "not implemented",
+	ErrCodeServiceUnavailable: "service unavailable",
+}
+
+func (e ErrCode) Message() string {
+	if msg, ok := errorMessages[e]; ok {
+		return msg
+	}
+	return "unknown error"
+}
